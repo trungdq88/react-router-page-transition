6b6c638fc3b9ba06c75008f2eec796708e85541b
Trung Dinh Quang <trungdq88@gmail.com>
2016-07-13 08:48:47 +0700

    Rename class

--- examples/material/js/App.jsx
+++ examples/material/js/App.jsx
@@ -3,14 +3,14 @@ import '../index.html';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Home.jsx';
+import Home from './Home.jsx';
 import ListPage from './ListPage.jsx';
 import ItemDetailPage from './ItemDetailPage.jsx';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 ReactDOM.render(
   <Router history={hashHistory}>
-    <Route path="/" component={App}>
+    <Route path="/" component={Home}>
       <IndexRoute component={ListPage} />
       <Route path="/detail/:itemId" component={ItemDetailPage} />
     </Route>
